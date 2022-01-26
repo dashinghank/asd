9a2846fd4606b88c5ff43388f660293f39dabe5c
unknown <dashing.hank@gmail.com>
2022-01-26 15:36:44 +0800

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,15 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
+    <div>
+
     <div className={`px-4 py-2 text-white bg-sky-700 hover:bg-sky-800 sm:px-8`}>asdasodkaosd</div>
+    <div className={`px-4 py-2 text-white bg-sky-700 hover:bg-sky-800 sm:px-8`}>asdasodkaosd</div>
+    <div className={`px-4 py-2 text-white bg-sky-700 hover:bg-sky-800 sm:px-8`}>asdasodkaosd</div>
+    <div className={`px-4 py-2 text-white bg-sky-700 hover:bg-sky-800 sm:px-8`}>asdasodkaosd</div>
+    <div className={`px-4 py-2 text-white bg-sky-700 hover:bg-sky-800 sm:px-8`}>asdasodkaosd</div>
+    <div className={`px-4 py-2 text-white bg-sky-700 hover:bg-sky-800 sm:px-8`}>asdasodkaosd</div>
+    </div>
   )
 }
 
